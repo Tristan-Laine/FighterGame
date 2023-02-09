c3a021d00aa00cc4c305afbe591694fd1b94cac4
tristanlaine <tristan.laine711@gmail.com>
2023-02-09 19:51:39 +0100

    sound effect menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,20 +5,27 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour{
     
     public GameObject settingsWindow;
+
+    public AudioSource audioSource;
+    public AudioClip sound;
     
     public void StartGame(){
+        audioSource.PlayOneShot(sound);
         SceneManager.LoadScene(3);
     }
 
     public void SettingsButton(){
+        audioSource.PlayOneShot(sound);
         settingsWindow.SetActive(true);  
     }
 
     public void CloseSettingsButton(){
+        audioSource.PlayOneShot(sound);
         settingsWindow.SetActive(false);  
     }
 
     public void QuitGame(){
+        audioSource.PlayOneShot(sound);
         Application.Quit();
     }
 }
--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -37,9 +37,14 @@ public class SettingsMenu : MonoBehaviour
         
     }
 
-    public void SetVolume(float volume)
+    public void SetMusicVolume(float volume)
     {
-        audioMixer.SetFloat("volume", volume);
+        audioMixer.SetFloat("Music", volume);
+    }
+
+      public void SetSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("Sound", volume);
     }
 
     public void SetFullScreen(bool isFullScreen)
